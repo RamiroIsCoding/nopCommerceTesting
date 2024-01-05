e2f81618d7d510dedbe1a37dd8c5b066aa9da622
ramiro <r.miguez.lauria@gmail.com>
2024-01-05 12:59:32 -0300

    implemented better practices, added one more wait to make tests more consistent

--- cypress/e2e/productsFilters-tests/filterByAttributesTests.cy.ts
+++ cypress/e2e/productsFilters-tests/filterByAttributesTests.cy.ts
@@ -9,7 +9,6 @@ describe("Check Notebooks filter by attributes", () => {
 })
 
 function applyEachFilterAndCheckAttributes() {
-  cy.intercept('GET', '**/category/products**').as('filterNetworkCall');
   cy.get(".filter-content .item")
     .each(($item, index) => {
       const filterName = $item.text().trim();
@@ -19,34 +18,37 @@ function applyEachFilterAndCheckAttributes() {
 }
 
 function applyFilter(filterIndex: number, filterName: string, typeOfFilter: string) {
+  cy.intercept('GET', '**/category/products**').as('filterNetworkCall');
   cy.get(".filter-content .item")
     .eq(filterIndex)
     .click()
-    .wait('@filterNetworkCall');
-
-  cy.url().then(urlWithFilters => {
-    checkEachProductForAttribute(urlWithFilters, filterName, typeOfFilter);
-    cy.get(".filter-content .item").eq(filterIndex).click()
-  });
+  cy.wait('@filterNetworkCall');
+  cy.get("div .ajax-products-busy")
+    .should('have.css', 'display', 'none');
+  cy.url()
+    .then(urlWithFilters => {
+      checkEachProductForAttribute(urlWithFilters, filterName, typeOfFilter);
+      cy.get(".filter-content .item").eq(filterIndex).click()
+    });
 }
 
 function checkEachProductForAttribute(urlWithFilters: string, filterName: string, typeOfFilter: string) {
-  cy.get(".product-item").each(($product, index) => {
-    cy.get(".product-item").eq(index).click().then(() => {
-      cy.get(".product-specs-box .spec-name")
-        .contains(typeOfFilter)
-        .parent()
-        .find(".spec-value")
-        .invoke("text")
-        .then((specValue) => {
-          cy.get(".product-name h1")
-            .invoke("text")
-            .then((productName) => {
-              expect(specValue, `${productName} should have spec: `).to.be.eq(filterName);
-            })
-        }).then(() => {
-        cy.visit(urlWithFilters);
-      })
+  cy.get(".product-item").each((_, index) => {
+    cy.get(".product-item").eq(index).click()
+
+    cy.get(".product-specs-box .spec-name")
+      .contains(typeOfFilter)
+      .parent()
+      .find(".spec-value")
+      .invoke("text")
+      .then((specValue) => {
+        cy.get(".product-name h1")
+          .invoke("text")
+          .then((productName) => {
+            expect(specValue, `${productName} should have spec: `).to.be.eq(filterName);
+          })
+      }).then(() => {
+      cy.visit(urlWithFilters);
     })
   })
 }
