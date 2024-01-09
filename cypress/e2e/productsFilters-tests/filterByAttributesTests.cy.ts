ca3a851e138d950b3a619cc01f9a5d35ea498f0b
ramiro <r.miguez.lauria@gmail.com>
2024-01-09 11:23:51 -0300

    improved the test functionality

--- cypress/e2e/productsFilters-tests/filterByAttributesTests.cy.ts
+++ cypress/e2e/productsFilters-tests/filterByAttributesTests.cy.ts
@@ -1,54 +1,80 @@
 describe("Check Notebooks filter by attributes", () => {
   beforeEach(() => {
     cy.visit("/notebooks");
+    cy.log("Visiting notebooks page");
   })
 
-  it.only("Check if the items contain the expected attribute", () => {
-    applyEachFilterAndCheckAttributes();
-  });
-})
+  it.only("verify if products contain attributes for each filter", () => {
+    const productInfo: Map<string, string[]> = new Map();
+    cy.log("Initializing product information map");
 
-function applyEachFilterAndCheckAttributes() {
-  cy.get(".filter-content .item")
-    .each(($item, index) => {
-      const filterName = $item.text().trim();
-      const typeOfFilter = $item.siblings(".name").text()
-      applyFilter(index, filterName, typeOfFilter);
-    });
-}
+    cy.get(".filter-content .item")
+      .each(($filter, index) => {
+        const filterName = $filter.text().trim();
+        applyFilter(index);
+        cy.log(`Applying filter: ${filterName}`);
 
-function applyFilter(filterIndex: number, filterName: string, typeOfFilter: string) {
-  cy.intercept('GET', '**/category/products**').as('filterNetworkCall');
-  cy.get(".filter-content .item")
-    .eq(filterIndex)
-    .click()
-  cy.wait('@filterNetworkCall');
-  cy.get("div .ajax-products-busy")
-    .should('have.css', 'display', 'none');
-  cy.url()
-    .then(urlWithFilters => {
-      checkEachProductForAttribute(urlWithFilters, filterName, typeOfFilter);
-      cy.get(".filter-content .item").eq(filterIndex).click()
-    });
-}
-
-function checkEachProductForAttribute(urlWithFilters: string, filterName: string, typeOfFilter: string) {
-  cy.get(".product-item").each((_, index) => {
-    cy.get(".product-item").eq(index).click()
-
-    cy.get(".product-specs-box .spec-name")
-      .contains(typeOfFilter)
-      .parent()
-      .find(".spec-value")
-      .invoke("text")
-      .then((specValue) => {
-        cy.get(".product-name h1")
-          .invoke("text")
-          .then((productName) => {
-            expect(specValue, `${productName} should have spec: `).to.be.eq(filterName);
-          })
-      }).then(() => {
-      cy.visit(urlWithFilters);
+        mapLinkWithComponents(filterName)
+          .then((mapLinkWithComponents: Map<string, string>) => {
+            mapLinkWithComponents.forEach((productComponent, productLink) => {
+              if (productInfo.has(productLink)) {
+                const info: string[] = productInfo.get(productLink);
+                info.push(filterName);
+                productInfo.set(productLink, info);
+              } else {
+                productInfo.set(productLink, [productComponent]);
+              }
+            })
+          });
+        unapplyFilter(index);
+      });
+    cy.then(() => {
+      assertEachProductContainsTheExpectedComponents(productInfo)
     })
   })
-}
+
+  function applyFilter(filterIndex: number) {
+    clickFilterAndWaitPageElementsToLoad(filterIndex);
+    cy.log("Applying filter")
+  }
+
+  function unapplyFilter(filterIndex: number) {
+    clickFilterAndWaitPageElementsToLoad(filterIndex);
+    cy.log("Unapply filter")
+  }
+
+  function clickFilterAndWaitPageElementsToLoad(filterIndex: number) {
+    cy.intercept('GET', '**/category/products**').as('filterNetworkCall');
+    cy.get(".filter-content .item")
+      .eq(filterIndex)
+      .click()
+    cy.wait('@filterNetworkCall');
+    cy.log("wait response");
+    cy.get("div .ajax-products-busy")
+      .should('have.css', 'display', 'none');
+  }
+
+  function mapLinkWithComponents(filterName: string) {
+    const component = filterName;
+    return cy.get(".product-item h2 > a")
+      .then((productsAnchor: JQuery) => {
+        const productLinkAndComponentMap: Map<string, string> = new Map();
+        productsAnchor.each((_, anchor: HTMLElement) => {
+          const link: string = anchor.getAttribute("href");
+          productLinkAndComponentMap.set(link, component);
+        })
+        return productLinkAndComponentMap;
+      })
+  }
+
+  function assertEachProductContainsTheExpectedComponents
+  (productsLinkComponentsMap: Map<string, string[]>) {
+    productsLinkComponentsMap.forEach((components: string[], link: string) => {
+      cy.visit(link);
+      components.forEach((value) => {
+        cy.get(".product-specs-box .spec-value").should("contain", value);
+      })
+    })
+  }
+})
+
